331c67ce29640e46e63d1957bf1ab3eb019dff77
melissa-24 <melissalongenberger24@gmail.com>
2021-05-25 20:34:32 -0400

    saving changes

--- shopApp/static/js/scripts.js
+++ shopApp/static/js/scripts.js
@@ -1,4 +1,15 @@
 const btn = document.querySelector('.dkMode');
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme == 'dark') {
+    document.body.classList.add('darkMode');
+}
+
 btn.addEventListener('click', function() {
     document.body.classList.toggle('darkMode');
+    let theme = 'light';
+    if (document.body.classList.contains('darkMode')) {
+        theme = 'dark'
+    }
+    localStorage.setItem('theme', theme);
 });

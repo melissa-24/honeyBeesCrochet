f346c92b470c93ddc3ce5323d1ffad39ac230ff9
dojo24 <melissalongenberger24@gmail.com>
2021-05-26 00:35:09 -0400

    modules, migrations, and base routes done

--- shopApp/models.py
+++ shopApp/models.py
@@ -1,36 +1,79 @@
 from django.db import models
 import re
 
+from django.db.models.deletion import CASCADE
+
 # ---------- User Tables ----------
 class Acct(models.Model):
-    pass
+    acctType = models.CharField(max_length=45, default='General')
 
 class UserManager(models.Manager):
-    pass
+    def validate(self, form):
+        errors = {}
+
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(form['email']):
+            errors['email'] = 'Invalid Email Address'
+
+        emailCheck = self.filter(email=form['email'])
+        if emailCheck:
+            errors['email'] = 'Email Address already in use'
+
+        usernameCheck = self.filter(username=form['username'])
+        if usernameCheck:
+            errors['username'] = 'Username already in use'
+
+        if len(form['password']) < 8:
+            errors['password'] = 'Password must be at least 8 characters'
+
+        if form['password'] != form['confirm']:
+            errors['password'] = 'Passwords do not match'
+
+        return errors
 
 class User(models.Model):
-    pass
+    firstName = models.CharField(max_length=45)
+    lastName = models.CharField(max_length=45)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=45)
+    password = models.CharField(max_length=45)
+    acct = models.ForeignKey(Acct, related_name='users', on_delete=CASCADE)
 
+    objects = UserManager()
+
+    userCreatedAt = models.DateTimeField(auto_now_add=True)
+    userUpdatedAt = models.DateTimeField(auto_now=True)
 
 
 # ---------- Hangouts Tables ----------
 class Topic(models.Model):
-    pass
+    topicName = models.CharField(max_length=255)
 
 class Post(models.Model):
-    pass
+    postTitle = models.CharField(max_length=255)
+    postContent = models.TextField()
+    postAuthor = models.ForeignKey(User, related_name='posts', on_delete=CASCADE)
+    postTopic = models.ForeignKey(Topic, related_name='topics', on_delete=CASCADE)
+    postCreatedAt = models.DateTimeField(auto_now_add=True)
+    postUpdatedAt = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
-    pass
+    commentContent = models.TextField()
+    commentAuthor = models.ForeignKey(User, related_name='comments', on_delete=CASCADE),
+    commentPost = models.ManyToManyField(Post, related_name='thePost')
+    commentCreatedAt = models.DateTimeField(auto_now_add=True)
+    commentUpdatedAt = models.DateTimeField(auto_now=True)
 
 
 
 # ---------- Products Tables ----------
 class Category(models.Model):
-    pass
-
-class OrderType(models.Model):
-    pass
+    catName = models.CharField(max_length=45)
 
 class Product(models.Model):
-    pass
+    itemName = models.CharField(max_length=45)
+    itemDescription = models.TextField()
+    itemPrice = models.CharField(max_length=45)
+    itemImg = models.CharField(max_length=255)
+    itemCount = models.CharField(max_length=45)
+    categories = models.ManyToManyField('Category', related_name='products')
--- shopApp/views.py
+++ shopApp/views.py
@@ -7,11 +7,6 @@ import bcrypt
 
 # ------ Main Landing Page ------
 def index(request):
-    # user = User.objects.get(id=request.session['user_id'])
-    # context = {
-    #     'user': user,
-    # }
-    # return render(request, 'index.html', context)
     return render(request, 'index.html')
 
 # ------ About Landing Page ------
@@ -32,15 +27,44 @@ def login(request):
 
 # ------ Login Route ------
 def access(request):
-    pass
+    user = User.objects.filter(username = request.POST['username'])
+    if user:
+        userLogin = user[0]
+        if bcrypt.checkpw(request.POST['password'].encode(), userLogin.password.encode()):
+            request.session['user_id'] = userLogin.id
+            return redirect('/dashboard/')
+        messages.error(request, 'Invalid Credentials')
+        return redirect('/login/')
+    messages.error(request, 'That Username is not in our system, please register for an account')
+    return redirect('/login/register/')
 
 # ------ Register Landing Page ------
 def register(request):
-    pass
+    context = {
+        'accts': Acct.objects.all()
+    }
+    return render(request, 'logReg/register.html', context)
 
 # ------ Register Route ------
 def signup(request):
-    pass
+    if request.method == 'GET':
+        return redirect('/login/register/')
+    errors = User.objects.validate(request.POST)
+    if errors:
+        for err in errors.values():
+            messages.error(request, err)
+        return redirect('/login/register/')
+    hashedPw = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
+    newUser = User.objects.create(
+        firstName = request.POST['firstName'],
+        lastName = request.POST['lastName'],
+        email = request.POST['email'],
+        username = request.POST['username'],
+        password = hashedPw,
+        acct_id=request.POST['acct']
+    )
+    request.session['user_id'] = newUser.id
+    return redirect('/dashboard/')
 
 # ------ Hangouts Landing Page ------
 def hangouts(request):
@@ -49,7 +73,7 @@ def hangouts(request):
 # ------ Hangouts Login Landing Page ------
 # Same table as regular log in just redirects back to hangouts
 def hangoutLogin(request):
-    pass
+    return render(request, 'logReg/hangoutsLogin.html')
 
 # ------ Hangouts Login Route ------
 def hangoutAccess(request):
@@ -57,32 +81,88 @@ def hangoutAccess(request):
 
 # ------ Hangouts Register Page ------
 def hangoutRegister(request):
-    pass
+    return render(request, 'logReg/hangoutsRegister.html')
 
 # ------ Hangouts Register Route ------
 def hangoutSignup(request):
     pass
 
+# ------ Accounts Landing Page ------
+def theAdmin(request):
+    context = {
+        'accts': Acct.objects.all().values()
+    }
+    return render(request, 'theAdmin.html', context)
+
+# ------ Create Acct Route ------
+def createAcct(request):
+    Acct.objects.create(
+        acctType=request.POST['acctType'],
+    )
+    return redirect('/theAdmin/')
+
+# ------ User Logout ------
+def logout(request):
+    request.session.clear()
+    return redirect('/')
+
+# ------------------------ Protected Pages ------------------------
+
+# ------ Dashboard Post Landing Page ------
+def dashboard(request):
+    if 'user_id' not in request.session:
+        return redirect('/login/')
+    user = User.objects.get(id=request.session['user_id'])
+    if user.acct_id == 1:
+        context = {
+            'user': user,
+        }
+        return render(request, 'protected/dashboard.html', context)
+    else:
+        context = {
+            'user': user,
+        }
+        return render(request, 'protected/adminDashboard.html', context)
 
 # ------------ Protected Admin Pages ------------
 
 # ------ Add to Store Landing Page ------
-def addItems(request):
+def addProduct(request):
+    return render(request, 'protected/admin/products.html')
+
+def addCategory(request):
+    return render(request, 'protected/admin/categories.html')
+
+def addTopic(request):
+    return render(request, 'protected/admin/topics.html')
+
+def viewUsers(request):
+    return render(request, 'protected/admin/users.html')
+
+def createProduct(request):
+    pass
+
+def viewAdminProduct(request):
     pass
 
 def createCategory(request):
     pass
 
-def createOrderType(request):
-    pass
-
-def createProduct(request):
+def createTopic(request):
     pass
 
 def assignCategory(request):
     pass
 
 # ------------ Protected Customer Pages ------------
+def profile(request):
+    pass
+
+def updateProfile(request):
+    pass
+
+def deleteProfile(request):
+    pass
 
 
 # ------------ Protected Hangout Pages ------------
@@ -91,11 +171,26 @@ def assignCategory(request):
 def addPost(request):
     pass
 
-def createTopic(request):
-    pass
-
 def createPost(request):
     pass
 
+def viewPost(request):
+    pass
+
+def updatePost(request):
+    pass
+
+def deletePost(request):
+    pass
+
 def createComment(request):
+    pass
+
+def viewComment(request):
+    pass
+
+def updateComment(request):
+    pass
+
+def deleteComment(request):
     pass
